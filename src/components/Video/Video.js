6b7599074facb96ddffaee0ea19eafded8cd85ba
JohnnyC <j0hnnywchiang@gmail.com>
2019-06-27 15:45:30 -0700

    title

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -9,16 +9,17 @@ export default function Video() {
     <div className="video-wrapper">
       {videos.map(video => (
         <div className="video-player">
+          <h3 className="video-title">{video.contentTitle}</h3>
           <MedalPlayer
             content={video}
             user={video.poster.displayName}
             videoOpts={{
-              autoplay: true, // should the video autoplay?
-              loop: true, // should the video loop?
-              muted: true, // is the video muted by default?
-              controls: true, // are the video controls enabled?
-              embedded: true, // is this an embedded player? should we include all branding components and enable player.js events?
-              retry: true // if the video fails to load, for whatever reason, retry video.play() up to 10 times
+              autoplay: false,
+              loop: true,
+              muted: true,
+              controls: true,
+              embedded: true,
+              retry: true
             }}
           />
         </div>
