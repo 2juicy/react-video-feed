9e9e1174d931d3dcb43bb2aea33292d4a4c467a9
JohnnyC <j0hnnywchiang@gmail.com>
2019-06-26 23:11:24 -0700

    some pseudo code setup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 function App() {
   return (
     <div className="App">
+      {/* Header Component Goes Here */}
       <header>
-        <h1>Nav</h1>
+        <h1>Nav section</h1>
       </header>
+      {/* Map videos here with title */}
       <main>
-        <h1>Main</h1>
+        <h1>Main section</h1>
       </main>
     </div>
   );
