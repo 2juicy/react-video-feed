b1858a7d0d03e54ef46e505ed131c343560cabd1
JohnnyC <j0hnnywchiang@gmail.com>
2019-06-27 21:26:15 -0700

    Updates

---
+++ precache-manifest.dd2967c9d88416c385549492b5f1631a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2705e74abfc63a571d5835ee9b8281c",
+    "url": "/medal-react-exercise/index.html"
+  },
+  {
+    "revision": "fc1e390aa34955cd5f49",
+    "url": "/medal-react-exercise/static/css/main.5d862539.chunk.css"
+  },
+  {
+    "revision": "3ffc435400841909d784",
+    "url": "/medal-react-exercise/static/js/2.2216a872.chunk.js"
+  },
+  {
+    "revision": "fc1e390aa34955cd5f49",
+    "url": "/medal-react-exercise/static/js/main.fe78c14e.chunk.js"
+  },
+  {
+    "revision": "1d737d8a3fe2a1707289",
+    "url": "/medal-react-exercise/static/js/runtime~main.b652e298.js"
+  }
+]);
