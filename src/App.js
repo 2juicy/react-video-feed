e8b2bbbd3069af293ce0c964f506a345dedaf7ee
JohnnyC <j0hnnywchiang@gmail.com>
2019-07-22 13:01:16 -0700

    refactor code

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
   useEffect(() => {
     (async function() {
       let res = await fetch("videos.json");
-      if (!res.ok) {
-        throw new Error("Failed to fetch");
-      }
+      if (!res.ok) throw new Error("Failed to fetch");
       let json = await res.json();
       setResults(json);
     })().catch(err => console.error(err));
